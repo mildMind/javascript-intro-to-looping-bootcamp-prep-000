0a7a8fd223e58a50c612730ec934832fd6b3df3a
Spencer D Stone <doomydoomdomm@gmail.com>
2018-10-05 22:09:47 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,12 @@
+var strange = new Array();
+
+function test() {
+  for (let i = 1; i<=25; i++){
+    if(strange.length = 0){
+      strange.push("I am 1 strange loop.")
+    }else{
+      strange.push("I am " + i + " strange loops.")
+    }
+    return strange
+  }
+}
